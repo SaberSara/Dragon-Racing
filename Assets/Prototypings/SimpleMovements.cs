5430d3f085e0638cd1eda6f84081c8dd5e604769
SaberSara <1992kurai@gmail.com>
2019-01-30 00:04:01 +0100

    Initital commit
    Added base folders and prototyping scene + third party prototyping materials + simpleMovements.cs Script

---
+++ Assets/Prototypings/SimpleMovements.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is for basic Prototype cube move
+/// </summary>
+public class SimpleMovements : MonoBehaviour
+{
+
+    //Attributs
+      private float speed;
+
+
+
+    //Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
